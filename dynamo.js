30d0d95d58716be873239d133ea83663ce46ea7c
yehiel amoyal <yehiel1460@gmail.com>
2022-10-02 14:27:03 +0300

    add try and catch

--- dynamo.js
+++ dynamo.js
@@ -1,18 +1,20 @@
 const AWS = require("aws-sdk");
 const dynamo = new AWS.DynamoDB();
+try {
+  const dynamoTables = async () => {
+    console.log("running dynamoTables function");
+    let tableParams = {};
+    const tableData = await dynamo.listTables(tableParams).promise();
 
-const dynamoTables = async () => {
-  console.log("running dynamoTables function");
-  let tableParams = {};
-  const tableData = await dynamo.listTables(tableParams).promise();
-
-  const tableList = tableData.TableNames.map(
-    (table) =>
-      `<a href="https://us-east-1.console.aws.amazon.com/dynamodbv2/home?region=us-east-1#item-explorer?initialTagKey=&table=${table}">${table}</a><br>`
-  ).join("");
-  return "<h1>Table List</h1>" + tableList;
-};
-
+    const tableList = tableData.TableNames.map(
+      (table) =>
+        `<a href="https://us-east-1.console.aws.amazon.com/dynamodbv2/home?region=us-east-1#item-explorer?initialTagKey=&table=${table}">${table}</a><br>`
+    ).join("");
+    return "<h1>Table List</h1>" + tableList;
+  };
+} catch (error) {
+  console.log(error);
+}
 module.exports = {
   dynamoTables: dynamoTables,
 };
--- lambda.js
+++ lambda.js
@@ -1,49 +1,52 @@
 const AWS = require("aws-sdk");
 const lambda = new AWS.Lambda();
+try {
+  const lambdaFunctions = async () => {
+    console.log("runing lambdaFunctions");
+    let params = {
+      MaxItems: 50,
+    };
 
-const lambdaFunctions = async () => {
-  console.log('runing lambdaFunctions');
-  let params = {
-    MaxItems: 50,
+    const allLambdas = [];
+    let data = { NextMarker: true };
+    while (data.NextMarker) {
+      data = await lambda.listFunctions(params).promise();
+      allLambdas.push(...data.Functions);
+      params.Marker = data.NextMarker;
+    }
+
+    allLambdas.sort((a, b) => {
+      const nameA = a.LastModified;
+      const nameB = b.LastModified;
+      if (nameA > nameB) return -1;
+      if (nameA < nameB) return 1;
+      return 0;
+    });
+
+    const appList = {};
+    for (const lambda of allLambdas) {
+      const firstHalf = lambda.Role.indexOf("/");
+      const secHalf = lambda.Role.indexOf("-");
+      const appName = lambda.Role.slice(firstHalf + 1, secHalf);
+      if (!appList[appName]) {
+        appList[appName] = [lambda.FunctionName];
+      } else {
+        appList[appName].push(lambda.FunctionName);
+      }
+    }
+
+    let lambdaHtml = "";
+    for (const [header, functions] of Object.entries(appList)) {
+      lambdaHtml += `<h1>${header}</h1><br>`;
+      for (const functionName of functions) {
+        lambdaHtml += `<a href="https://us-east-1.console.aws.amazon.com/lambda/home?region=us-east-1#/functions/${functionName}">${functionName}<a/> - <a href= "https://us-east-1.console.aws.amazon.com/cloudwatch/home?region=us-east-1#logsV2:log-groups/log-group/$252Faws$252Flambda$252F${functionName}/log-events">logs<a/><br>`;
+      }
+    }
+    return lambdaHtml;
   };
-
-  const allLambdas = [];
-  let data = { NextMarker: true };
-  while (data.NextMarker) {
-    data = await lambda.listFunctions(params).promise();
-    allLambdas.push(...data.Functions);
-    params.Marker = data.NextMarker;
-  }
-
-  allLambdas.sort((a, b) => {
-    const nameA = a.LastModified;
-    const nameB = b.LastModified;
-    if (nameA > nameB) return -1;
-    if (nameA < nameB) return 1;
-    return 0;
-  });
-
-  const appList = {};
-  for (const lambda of allLambdas) {
-    const firstHalf = lambda.Role.indexOf("/");
-    const secHalf = lambda.Role.indexOf("-");
-    const appName = lambda.Role.slice(firstHalf + 1, secHalf);
-    if (!appList[appName]) {
-      appList[appName] = [lambda.FunctionName];
-    } else {
-      appList[appName].push(lambda.FunctionName);
-    }
-  }
-
-  let lambdaHtml = "";
-  for (const [header, functions] of Object.entries(appList)) {
-    lambdaHtml += `<h1>${header}</h1><br>`;
-    for (const functionName of functions) {
-      lambdaHtml += `<a href="https://us-east-1.console.aws.amazon.com/lambda/home?region=us-east-1#/functions/${functionName}">${functionName}<a/> - <a href= "https://us-east-1.console.aws.amazon.com/cloudwatch/home?region=us-east-1#logsV2:log-groups/log-group/$252Faws$252Flambda$252F${functionName}/log-events">logs<a/><br>`;
-    }
-  }
-  return lambdaHtml;
-};
+} catch (error) {
+  console.log(error);
+}
 module.exports = {
   lambdaFunctions: lambdaFunctions,
 };
