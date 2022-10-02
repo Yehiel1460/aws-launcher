947dc2925d6cc6061e575dbafb18a32eaa92f5a8
yehiel amoyal <yehiel1460@gmail.com>
2022-10-02 11:23:28 +0300

    Update index.js

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ exports.handler = async (event) => {
   console.log({ event });
   const viewValue = {
     view: event.queryStringParameters.view,
+    domainName: event.domainName.domainName,
+    path: event.path.path,
   };
 
   let viewResault;
@@ -12,7 +14,7 @@ exports.handler = async (event) => {
   } else if (viewValue.view === "lambda") {
     viewResault = await lambdaFunctions();
   } else {
-    viewResault = `<a href= "https://yv8insn58f.execute-api.us-east-1.amazonaws.com/default/aws-launcher-test?view=dynamo">Dynamo List</a> - <a href= "https://yv8insn58f.execute-api.us-east-1.amazonaws.com/default/aws-launcher-test?view=lambda">Lambda List</a>`
+    viewResault = `<a href= ${viewValue.domainName + viewValue.path + '?view=dynamo'}>Dynamo List</a> - <a href= "https://yv8insn58f.execute-api.us-east-1.amazonaws.com/default/aws-launcher-test?view=lambda">Lambda List</a>`;
   }
 
   const response = {
