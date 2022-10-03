b32b080a1ec46b6bc94437b8017e894e8c329681
yehiel amoyal <yehiel1460@gmail.com>
2022-10-03 13:52:42 +0300

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,14 @@ try {
     console.log({ event });
     const viewValue = {
       view: event.queryStringParameters?.view,
+      AWS_ACCESS_KEY_ID: event.queryStringParameters?.AWS_ACCESS_KEY_ID,
+      AWS_SECRET_ACCESS_KEY: event.queryStringParameters?.AWS_SECRET_ACCESS_KEY
     };
+    // process.env.AWS_ACCESS_KEY_ID = viewValue.AWS_ACCESS_KEY_ID
+    // process.env.AWS_SECRET_ACCESS_KEY = viewValue.AWS_SECRET_ACCESS_KEY
+
+    console.log(process.env.AWS_ACCESS_KEY_ID = viewValue.AWS_ACCESS_KEY_ID);
+    console.log(process.env.AWS_SECRET_ACCESS_KEY = viewValue.AWS_SECRET_ACCESS_KEY);
 
     let viewResault;
     if (viewValue.view === "dynamo") {
