d1fbe0318e67848100f34dd976c8b0f4c6d643cb
yehiel amoyal <yehiel1460@gmail.com>
2022-10-06 13:51:49 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,44 +1,30 @@
-const { dynamoTables } = require("./dynamo.js");
-const { lambdaFunctions } = require("./lambda.js");
-try {
-  exports.handler = async (event) => {
-    console.log({ event });
-    const viewValue = {
-      view: event.queryStringParameters?.view,
-      AWS_ACCESS_KEY_ID: event.queryStringParameters?.AWS_ACCESS_KEY_ID,
-      AWS_SECRET_ACCESS_KEY: event.queryStringParameters?.AWS_SECRET_ACCESS_KEY
-    };
-    // process.env.AWS_ACCESS_KEY_ID = viewValue.AWS_ACCESS_KEY_ID
-    // process.env.AWS_SECRET_ACCESS_KEY = viewValue.AWS_SECRET_ACCESS_KEY
-
-    console.log(process.env.AWS_ACCESS_KEY_ID = viewValue.AWS_ACCESS_KEY_ID);
-    console.log(process.env.AWS_SECRET_ACCESS_KEY = viewValue.AWS_SECRET_ACCESS_KEY);
-
-    let viewResault;
-    if (viewValue.view === "dynamo") {
-      viewResault = await dynamoTables();
-    } else if (viewValue.view === "lambda") {
-      viewResault = await lambdaFunctions();
-    } else if (!event.queryStringParameters) {
-      viewResault = `<a href= ?view=dynamo>Dynamo List</a> - <a href= ?view=lambda>Lambda List</a>`;
-    }
-
-    const response = {
-      statusCode: 200,
-      body: viewResault,
-      headers: {
-        "Content-Type": "text/html",
-      },
-    };
-    return response;
+const AWS = require("aws-sdk");
+exports.handler = async (event) => {
+  AWS.config.update({
+    MasterRegion: "us-east-1",
+    accessKeyId: event.queryStringParameters.accessKeyId,
+    secretAccessKey: event.queryStringParameters.secretAccessKey,
+  });
+  const lambda = new AWS.Lambda();
+  const params = {
+    FunctionVersion: "ALL",
+    // Marker: 'STRING_VALUE',
+    // MasterRegion: "us-east-1",
+    MaxItems: 50,
   };
-} catch (error) {
+let data
+  const getLambdas = async () => {
+    data = await lambda.listFunctions(params).promise();
+    console.log(data);
+    return data
+  };
+
+  await getLambdas(params);
   const response = {
     statusCode: 200,
-    body: `<p>${error}</p>`,
-    headers: {
-      "Content-Type": "text/html",
-    },
+    body: JSON.stringify(data.Functions.map((func)=>{
+      return (func.FunctionName)
+    }))
   };
-  return response
+  return response;
 }
