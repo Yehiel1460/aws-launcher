93ed2e3160e940bdbcb03171aaed28077c14e30d
yehiel amoyal <yehiel1460@gmail.com>
2022-10-11 12:36:30 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 const AWS = require("aws-sdk");
 const { dynamo } = require("./dynamo.js");
 const { lambda } = require("./lambda.js");
-const { pipeline } = require("./pipeline.js");
 try {
   exports.handler = async (event) => {
     console.log({ event });
@@ -40,9 +39,8 @@ const getViewResault = async (accessKeyId, secretAccessKey, view) => {
   if (!accessKeyId) {
     return "<p>Please enter accessKeyId and secretAccessKey</p>";
   }
-  console.log({ view });
   return (
-    (await { dynamo, lambda, pipeline }[view](AWS)) ||
+    (await { dynamo, lambda }[view](AWS)) ||
     `<a href= ?view=dynamo&accessKeyId=${accessKeyId}&secretAccessKey=${secretAccessKey}>Dynamo List</a> - <a href= ?view=lambda&accessKeyId=${accessKeyId}&secretAccessKey=${secretAccessKey}>Lambda List</a>`
   );
 };
