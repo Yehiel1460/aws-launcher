f665ac8d02c559768db7f9b8bf8d6913870d7f44
yehiel amoyal <yehiel1460@gmail.com>
2022-10-12 10:25:40 +0300

    Update index.js

--- index.js
+++ index.js
@@ -2,21 +2,20 @@ const AWS = require("aws-sdk");
 const { dynamo } = require("./dynamo.js");
 const { lambda } = require("./lambda.js");
 const { pipeline } = require("./pipeline.js");
-try {
-  exports.handler = async (event) => {
-    console.log({ event });
-    const viewValue = {
-      view: 'pipeline',
-    };
-    const accessKeyId = 'AKIAVNP7MCLIPBQGQPIA';
-    const secretAccessKey = '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';
-    console.log('before');
-    AWS.config.update({
-      MasterRegion: "us-east-1",
-      accessKeyId: accessKeyId,
-      secretAccessKey: secretAccessKey,
-    });
-    
+
+exports.handler = async (event) => {
+  console.log({ event });
+  const viewValue = {
+    view: event.queryStringParameters.view,
+  };
+  const accessKeyId = event.queryStringParameters.accessKeyId;
+  const secretAccessKey = event.queryStringParameters.secretAccessKey;
+  AWS.config.update({
+    MasterRegion: "us-east-1",
+    accessKeyId: accessKeyId,
+    secretAccessKey: secretAccessKey,
+  });
+  try {
     const response = {
       statusCode: 200,
       body: await getViewResult(accessKeyId, secretAccessKey, viewValue.view),
@@ -24,21 +23,23 @@ try {
         "Content-Type": "text/html",
       },
     };
-    return response;
-  };
-} catch (error) {
-  const response = {
-    statusCode: 200,
-    body: `<p>${error}</p>`,
-    headers: {
-      "Content-Type": "text/html",
-    },
-  };
   return response;
-}
+  } catch (error) {
+    const response = {
+      statusCode: 200,
+      body: `<p>${error}</p>`,
+      headers: {
+        "Content-Type": "text/html",
+      },
+    };
+    return response;
+  }
+};
+
 const getDefaultHtml = (listType, accessKeyId, secretAccessKey) => {
   let defaultHtml;
   for (const [type] of Object.entries(listType)) {
+    console.log(type)
     defaultHtml += `<a href= ?view=${type}&accessKeyId=${accessKeyId}&secretAccessKey=${secretAccessKey}>${type} List</a> - `;
   }
   return defaultHtml;
