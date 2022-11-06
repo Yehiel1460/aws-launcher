c5893fd76d25bdf32db4527fbfbb0d98ac2309b3
yehiel amoyal <yehiel1460@gmail.com>
2022-11-06 16:57:20 +0200

    Update client.js

--- client.js
+++ client.js
@@ -11,7 +11,6 @@ const client = async (typeName, listType,accessKeyId,secretAccessKey) => {
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <link rel='stylesheet' type='text/css' media='screen' href='style.css'>
   <title>aws-launcher</title>
   </head>
   <body>
