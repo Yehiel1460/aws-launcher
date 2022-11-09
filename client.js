39ef15722ee5449065c670327ac7bc72c6994f0d
yehiel amoyal <yehiel1460@gmail.com>
2022-11-09 11:51:20 +0200

    Update client.js

--- client.js
+++ client.js
@@ -71,9 +71,10 @@ const getDefaultHtml = (listType, accessKeyId, secretAccessKey,typeName) => {
 
 const getViewResult = async (accessKeyId, secretAccessKey, view) => {
   if (!accessKeyId || !secretAccessKey) {
-    return `<form onsubmit="window.location = window.location + &accessKeyId=accessKeyId.value&secretAccessKey=encodeURIComponent(secretAccessKey.value)">
-    <input id="accessKeyId" placeholder="Enter Access Key Id" type="search" name="accessKeyId"></input>
-    <input id="secretAccessKey" placeholder="Enter Secret Access Key" type="search" name="secretAccessKey"></input>
+    return `<form onsubmit="window.location = window.location + ?view="view.value"&accessKeyId=accessKeyId.value&secretAccessKey=encodeURIComponent(secretAccessKey.value)">
+    <input style="display:none;" id="view" value="lambda" type="search" name="view"></input>
+    <input id="accessKeyId" placeholder="Enter Access Key Id" type="search" name="accessKeyId" required></input>
+    <input id="secretAccessKey" placeholder="Enter Secret Access Key" type="search" name="secretAccessKey" required></input>
     <button>Login</button>
     </form>`;
   }
